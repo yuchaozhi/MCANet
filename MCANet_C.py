504859953895d4007f029e5567e1069d5ca58d80
Yu Chaozhi <140902387+yuchaozhi@users.noreply.github.com>
2024-12-10 17:54:03 +0800

    Create MCANet_C.py

---
+++ MCANet_C.py
@@ -0,0 +1,236 @@
+from unet_parts import *
+
+
+class PMA(nn.Module):
+
+    def __init__(self, channels, factor=8):
+        super(PMA, self).__init__()
+        self.groups = factor
+        assert channels // self.groups > 0
+        self.softmax = nn.Softmax(-1)
+        self.agp = nn.AdaptiveAvgPool2d((1, 1))
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        self.gn = nn.GroupNorm(channels // self.groups,
+                               channels // self.groups)
+        self.conv1x1 = nn.Conv2d(channels // self.groups,
+                                 channels // self.groups,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.conv3x3 = nn.Conv2d(channels // self.groups,
+                                 channels // self.groups,
+                                 kernel_size=3,
+                                 stride=1,
+                                 padding=1)
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+        group_x = x.reshape(b * self.groups, -1, h, w)  # b*g,c//g,h,w
+        x_h = self.pool_h(group_x)
+        x_w = self.pool_w(group_x).permute(0, 1, 3, 2)
+        hw = self.conv1x1(torch.cat([x_h, x_w], dim=2))
+        x_h, x_w = torch.split(hw, [h, w], dim=2)
+        x1 = self.gn(group_x * x_h.sigmoid() *
+                     x_w.permute(0, 1, 3, 2).sigmoid())
+        x2 = self.conv3x3(group_x)
+        x11 = self.softmax(
+            self.agp(x1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x12 = x2.reshape(b * self.groups, c // self.groups,
+                         -1)  # b*g, c//g, hw
+        x21 = self.softmax(
+            self.agp(x2).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x22 = x1.reshape(b * self.groups, c // self.groups,
+                         -1)  # b*g, c//g, hw
+        weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(
+            b * self.groups, 1, h, w)
+        return (group_x * weights.sigmoid()).reshape(b, c, h, w)
+
+
+class BasicConv(nn.Module):
+
+    def __init__(self,
+                 in_planes,
+                 out_planes,
+                 kernel_size,
+                 stride=1,
+                 padding=0,
+                 dilation=1,
+                 groups=1,
+                 relu=True,
+                 bn=True,
+                 bias=False):
+        super(BasicConv, self).__init__()
+        self.out_channels = out_planes
+        self.conv = nn.Conv2d(in_planes,
+                              out_planes,
+                              kernel_size=kernel_size,
+                              stride=stride,
+                              padding=padding,
+                              dilation=dilation,
+                              groups=groups,
+                              bias=bias)
+        self.bn = nn.BatchNorm2d(
+            out_planes, eps=1e-5, momentum=0.01, affine=True) if bn else None
+        self.relu = nn.ReLU() if relu else None
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.bn is not None:
+            x = self.bn(x)
+        if self.relu is not None:
+            x = self.relu(x)
+        return x
+
+
+class ZPool(nn.Module):
+
+    def forward(self, x):
+        return torch.cat(
+            (torch.max(x, 1)[0].unsqueeze(1), torch.mean(x, 1).unsqueeze(1)),
+            dim=1)
+
+
+class AttentionGate(nn.Module):
+
+    def __init__(self):
+        super(AttentionGate, self).__init__()
+        kernel_size = 7
+        self.compress = ZPool()
+        self.conv = BasicConv(2,
+                              1,
+                              kernel_size,
+                              stride=1,
+                              padding=(kernel_size - 1) // 2,
+                              relu=False)
+
+    def forward(self, x):
+        x_compress = self.compress(x)
+        x_out = self.conv(x_compress)
+        scale = torch.sigmoid_(x_out)
+        return x * scale
+
+
+class CDA(nn.Module):
+
+    def __init__(self, no_spatial=False):
+        super(CDA, self).__init__()
+        self.cw = AttentionGate()
+        self.hc = AttentionGate()
+        self.no_spatial = no_spatial
+        if not no_spatial:
+            self.hw = AttentionGate()
+
+    def forward(self, x):
+        x_perm1 = x.permute(0, 2, 1, 3).contiguous()
+        x_out1 = self.cw(x_perm1)
+        x_out11 = x_out1.permute(0, 2, 1, 3).contiguous()
+        x_perm2 = x.permute(0, 3, 2, 1).contiguous()
+        x_out2 = self.hc(x_perm2)
+        x_out21 = x_out2.permute(0, 3, 2, 1).contiguous()
+        if not self.no_spatial:
+            x_out = self.hw(x)
+            x_out = 1 / 3 * (x_out + x_out11 + x_out21)
+        else:
+            x_out = 1 / 2 * (x_out11 + x_out21)
+        return x_out
+
+
+class Mlp(nn.Module):
+
+    def __init__(self,
+                 in_features,
+                 hidden_features=None,
+                 out_features=None,
+                 act_layer=nn.GELU,
+                 drop=0.):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        self.fc1 = nn.Linear(in_features, hidden_features)
+        self.act = act_layer()
+        self.fc2 = nn.Linear(hidden_features, out_features)
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        x = self.drop(x)
+        return x
+
+
+class MCANet_C(nn.Module):
+
+    def __init__(self, n_channels, n_classes, bilinear=True):
+        super(MCANet_C, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(1024, 256 // factor, bilinear)
+        self.up3 = Up(512, 128 // factor, bilinear)
+        self.up4 = Up(256, 64, bilinear)
+        self.outc = OutConv(128, n_classes)
+
+        self.trip = CDA()
+        self.ema = PMA(512)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        '''Setting 1'''
+        print(x5.shape)
+        x5_trip = self.trip(x5)
+        '''Setting 2'''
+
+        x5_ema = self.ema(x5)
+
+        x5 = x5_trip + x5_ema + x5
+
+        x5_scale = F.interpolate(x5,
+                                 size=[256, 256],
+                                 mode='bilinear',
+                                 align_corners=True)
+
+        x6 = self.up1(x5, x4)
+        x6_scale = F.interpolate(x6,
+                                 size=[256, 256],
+                                 mode='bilinear',
+                                 align_corners=True)
+
+        x7 = self.up2(x6, x3)
+        x7_scale = F.interpolate(x7,
+                                 size=[256, 256],
+                                 mode='bilinear',
+                                 align_corners=True)
+
+        x8 = self.up3(x7, x2)
+        x8_scale = F.interpolate(x8,
+                                 size=[256, 256],
+                                 mode='bilinear',
+                                 align_corners=True)
+
+        x9 = self.up4(x8, x1)
+
+        x9 = torch.cat((x9, x8_scale, x7_scale, x6_scale, x5_scale), 1)
+
+        logits = self.outc(x9)
+        return logits
+
+
+if __name__ == '__main__':
+    model_ = MCANet_C(n_channels=3, n_classes=1)
+    a = torch.rand(size=(8, 3, 256, 256))
+    b = model_(a)
+    print(b.shape)
